76db259a90450d5f90bbdb2c9e1fbd5be29afec7
dicky ardiar <diackyardiar1@gmail.com>
2024-06-02 21:29:18 +0700

    fixing issue tailwind

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Wrapper from "./molecules/global/wrapper";
 
--- dashboard/src/molecules/global/wrapper.jsx
+++ dashboard/src/molecules/global/wrapper.jsx
@@ -1,12 +1,12 @@
 import Navigation from "../../components/navbar"
-import Sidebar from "../../components/Sidebar"
+import { SidebarMenu } from "../../components/Sidebar"
 import MainMenu from "../../containers/MainMenu"
 
 const Wrapper = () => {
   return (
     <div className="flex min-h-screen overflow-hidden relative">
       <div className="w-60 bg-gray-100 border-r">
-        <Sidebar/>
+        <SidebarMenu />
       </div>
 
       <div className="flex flex-col flex-1 overflow-hidden">
